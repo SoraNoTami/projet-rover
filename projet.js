a22b0f467859ce68bfada5cc3364be15f6b8c2d3
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-02-09 12:24:02 +0100

    Finish Bonus 10

--- projet.js
+++ projet.js
@@ -2,15 +2,15 @@ const prompt = require("prompt");
 
 let grid = [
 	/*0*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*1*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*2*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*3*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*4*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*5*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*6*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*7*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	/*8*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-    /*9*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+	/*-1*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	/*-2*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	/*-3*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	/*-4*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	/*-5*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	/*-6*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	/*-7*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	/*-8*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    /*-9*/[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 
 let rover = {
@@ -45,34 +45,78 @@ function turnRight() {
     } else if (rover.direction === "W") {
         rover.direction = "N";
     } else {
-        console.error();
+        return console.error();
     }
 }
 
 function moveForward() {
     if (rover.direction === "S") {
-        rover.y -= 1;
+        if (rover.y - 1 > -10) {
+            rover.y -= 1;
+        } else {
+            console.error('error: this is a wall');
+        }
     } else if (rover.direction === "N") {
-        rover.y += 1;
+        if (rover.y + 1 < 1) {
+            rover.y += 1;
+        } else {
+            console.error('error: this is a wall');
+        }
     } else if (rover.direction === "E") {
-        rover.x += 1;
+        if (rover.x + 1 < 10) {
+            rover.x += 1;
+        } else {
+            console.error('error: this is a wall');
+        }
     } else if (rover.direction === "W") {
-        rover.x -= 1;
+        if (rover.x - 1 > -1) {
+            rover.x -= 1;
+        } else {
+            console.error('error: this is a wall');
+        }
+    }
+}
+
+function moveBackward() {
+    if (rover.direction === "S") {
+        if (rover.y + 1 < 1) {
+            rover.y += 1;
+        } else {
+            console.error('error: this is a wall');
+        }
+    } else if (rover.direction === "N") {
+        if (rover.y - 1 > -10) {
+            rover.y -= 1;
+        } else {
+            console.error('error: this is a wall');
+        }
+    } else if (rover.direction === "E") {
+        if (rover.x - 1 > -1) {
+            rover.x -= 1;
+        } else {
+            console.error('error: this is a wall');
+        }
+    } else if (rover.direction === "W") {
+        if (rover.x + 1 < 10) {
+            rover.x += 1;
+        } else {
+            console.error('error: this is a wall');
+        }
     }
 }
 
 let yourDirection = {
     properties: {
         direction: {
-            pattern: /^[rlf]{1,}$/,
-            message: 'we need a l or r or f without space',
+            pattern: /^[rlfb]{1,}$/,
+            message: 'we only need l or r or f or b without space',
             required: true
         },
-	}
+    }
 };
 
 function pilotRover(guid) {
-    for (i = 0; i < guid.length; i++){
+    for (i = 0; i < guid.length; i++) {
         // console.log(guid.charAt(i))
         if (guid.charAt(i) === "l") {
             turnLeft();
@@ -82,11 +126,16 @@ function pilotRover(guid) {
         }
         else if (guid.charAt(i) === "f") {
             moveForward();
-        }else{
-            console.error("we need a l or r or f without space")
         }
-        console.log(rover)
+        else if (guid.charAt(i) === "b") {
+            moveBackward();
+        }
+        else {
+            return console.error()
+        }
         rover.travelLog.push(guid.charAt(i))
+        console.log(rover)
+        
     }
 
 }
